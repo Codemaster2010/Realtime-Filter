c9ba91c7a780e62d17431d4f9642e82da99e28a0
Codemaster2010 <81978032+Codemaster2010@users.noreply.github.com>
2021-08-30 12:00:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+function setup(){
+    canavs= createCanvas(300, 300);
+    canavs.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
